b53d9a7c112f73c903d20152891095c4cc6e8188
Garrick Wong <inquisitor8986@gmail.com>
2024-09-06 19:36:57 -0600

    A bit broken, but will fix soon

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ class guiFrame(wx.Frame):
         panel = wx.Panel(self)
         self.InitUI(panel)
         self.msgToEncode(panel)
+        self.msgToDecode(panel)
         self.info = {1:'', 2:'', 3:'', 4: '', 5: '', 6: ''}
         self.clicked = False
         self.message = ''
@@ -14,7 +15,7 @@ class guiFrame(wx.Frame):
     def InitUI(self,panel):
         self.comboBox(panel)
         self.decodeComboBox(panel)
-        self.SetSize((300, 200))
+        self.SetSize((300, 250))
         self.SetTitle('Message box')
         self.Centre()
 
@@ -33,38 +34,24 @@ class guiFrame(wx.Frame):
         box1.Bind(wx.EVT_COMBOBOX, self.box1Triggered)
         box2.Bind(wx.EVT_COMBOBOX, self.box2Triggered)
 
-    def decodeComboBox(self, panel):
-        alphabet = list("abcdefghijklmnopqrstuvwxyz")
-        box3 = wx.ComboBox(panel, pos=(5,105), choices = alphabet, style=wx.CB_READONLY)
-        box4 = wx.ComboBox(panel, pos=(55,105), choices = alphabet, style=wx.CB_READONLY)
-        box3.Bind(wx.EVT_COMBOBOX, self.box3Triggered)
-        box4.Bind(wx.EVT_COMBOBOX, self.box4Triggered)
-
     def box1Triggered(self, guiFrame):
         letter1 = guiFrame.GetString()
         self.info[1] = letter1
         print(self.clicked)
+        print(self.info)
 
     def box2Triggered(self, guiFrame):
         letter2 = guiFrame.GetString()
         self.info[2] = letter2
         print(self.clicked)
-
-    def box3Triggered(self, guiFrame):
-        letter3 = guiFrame.GetString()
-        self.info[4] = letter3
-        print(self.clicked)
-
-    def box4Triggered(self, guiFrame):
-        letter4 = guiFrame.GetString()
-        self.info[5] = letter4
-        print(self.clicked)
+        print(self.info)
 
     def ctrlBoxTriggered(self, event):
         msg = self.text_ctrl.GetValue()
         self.clicked = True
         self.info[3] = msg
         print(self.clicked)
+        print(self.info)
         if self.info[3] == "Guess What": #Easter Egg Fun
             self.message = "Chicken Butt"
         else:
@@ -72,6 +59,44 @@ class guiFrame(wx.Frame):
         print(self.message, msg)
         wx.CallLater(1000, self.ShowMessage)
 
+
+     ###### DECODING ########
+    def msgToDecode(self, panel): #Temp for message submission
+        self.text_ctrl = wx.TextCtrl(panel, pos = (5,140))
+        self.button = wx.Button(panel, label = "Decode", pos = (5,165))
+        self.button.Bind(wx.EVT_BUTTON, self.decodeBoxTriggered)   
+
+    def decodeComboBox(self, panel):
+        alphabet = list("abcdefghijklmnopqrstuvwxyz")
+        box3 = wx.ComboBox(panel, pos=(5,105), choices = alphabet, style=wx.CB_READONLY)
+        box4 = wx.ComboBox(panel, pos=(55,105), choices = alphabet, style=wx.CB_READONLY)
+        box3.Bind(wx.EVT_COMBOBOX, self.box3Triggered)
+        box4.Bind(wx.EVT_COMBOBOX, self.box4Triggered)
+
+    def box3Triggered(self, guiFrame):
+        letter3 = guiFrame.GetString()
+        self.info[4] = letter3
+        print(self.clicked)
+        print(self.info)
+
+    def box4Triggered(self, guiFrame):
+        letter4 = guiFrame.GetString()
+        self.info[5] = letter4
+        print(self.clicked)
+        print(self.info)
+
+    def decodeBoxTriggered(self, event):
+        msg = self.text_ctrl.GetValue()
+        self.clicked = True
+        self.info[6] = msg
+        print(self.clicked)
+        print(self.info)
+        self.message = ceasarCipher.decoding(self.info)
+        print(self.message, msg)
+        wx.CallLater(1000, self.ShowDecoded)
+
+    def ShowDecoded(self):
+        wx.MessageBox(self.message, 'Decoded Message', wx.OK | wx.ICON_INFORMATION)
 
 def main():
     app = wx.App()
